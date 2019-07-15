cb673af3187defc9ba6cf03f7a2bcc3b4ac65209
Ivan Radkevich <ivanradkevich@yahoo.com>
2019-07-15 14:09:57 +0300

    Initial Commit

---
+++ ttbl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ttbl
+//
+//  Created by Ivan Radkevich on 7/15/19.
+//  Copyright © 2019 Ivan Radkevich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
